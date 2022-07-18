c467e706ce5a7d67a43d2cadeb632b13e6314367
Omar-Raslan <medo.selim20@gmail.com>
2022-07-18 23:11:32 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -19,3 +19,19 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+//About slider
+let items = document.querySelectorAll('#sliderButton .carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
